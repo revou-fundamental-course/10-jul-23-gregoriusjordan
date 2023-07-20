edfb7dd092718d36af092f9ed88777ef23800a19
Gregorius Jordan <81107981+gregoriusjordan@users.noreply.github.com>
2023-07-21 00:21:00 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
- // Function to calculate the BMI
  function calculateBMI() {
     const age = parseInt(document.getElementById('age').value);
     const height = parseFloat(document.getElementById('height').value);
     const weight = parseFloat(document.getElementById('weight').value);
     const gender = document.querySelector('input[name="radio"]:checked').value;
 
-    // Check if all fields have valid inputs
     if (isNaN(age) || isNaN(height) || isNaN(weight) || !gender) {
         alert('Please fill all the fields with valid input.');
         return;
     }
 
-    // Calculate BMI
+
     const bmi = weight / ((height / 100) ** 2);
     const roundedBMI = bmi.toFixed(2);
-
-    // Display the BMI result
     const bmiResult = document.querySelector('.id');
     bmiResult.textContent = roundedBMI;
 
-    // Provide a comment based on BMI category
     const comment = document.querySelector('.comment');
     if (bmi < 18.5) {
         comment.textContent = 'Underweight';
@@ -43,12 +38,10 @@ document.querySelector('input[name="radio"]:checked').checked = false;
 const bmiResult = document.querySelector('.id');
 bmiResult.textContent = '';
 
-   
 }
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Your JavaScript code, including the event listener setup, goes here
     const calculateButton = document.getElementById('submit');
     calculateButton.addEventListener('click', calculateBMI);
   });
